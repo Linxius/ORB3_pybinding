deed3d77b63d6acc5218041560b03d7b2457307a
Linxius <liniquie@gmail.com>
2021-07-03 16:02:48 +0800

    add slam step

--- src/slam.cc
+++ src/slam.cc
@@ -1,335 +1,38 @@
+#include "System.h"
+#include "cvt.h"
+#include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgcodecs/imgcodecs.hpp>
 #include <pybind11/numpy.h>
 #include <pybind11/pybind11.h>
 
-#include "cvt.h"
-
 namespace py = pybind11;
 
-#include <algorithm>
-#include <chrono>
-#include <fstream>
-#include <iostream>
-#include <stdlib.h>
-
-#include <opencv2/core/core.hpp>
-#include <opencv2/imgcodecs/imgcodecs.hpp>
-
-#include <System.h>
-
-using namespace std;
-
-#include <windows.h>
-
-static void usleep(__int64 usec) {
-  HANDLE timer;
-  LARGE_INTEGER ft;
-
-  ft.QuadPart = -(10 * usec); // Convert to 100 nanosecond interval, negative
-                              // value indicates relative time
-
-  timer = CreateWaitableTimer(NULL, TRUE, NULL);
-  SetWaitableTimer(timer, &ft, 0, NULL, NULL, 0);
-  WaitForSingleObject(timer, INFINITE);
-  CloseHandle(timer);
-}
-
-void LoadImages(const string &strImagePath, const string &strPathTimes,
-                vector<string> &vstrImages, vector<double> &vTimeStamps);
-
-//.\Monocular\Release\mono_euroc.exe
-// ..\Vocabulary\ORBvoc.txt
-//.\Monocular\EuRoC.yaml
-// ..\data\MH_01_easy\
-//./Monocular/EuRoC_TimeStamps/MH01.txt
-// dataset-MH01_mono
 #define ARG1 "extern/ORB_SLAM3/Vocabulary/ORBvoc.txt"
 #define ARG2 "extern/ORB_SLAM3/Examples/Monocular/EuRoC.yaml"
-#define ARG3 "extern/ORB_SLAM3/data/MH_01_easy"
-#define ARG4 "extern/ORB_SLAM3/Examples/Monocular/EuRoC_TimeStamps/MH01.txt"
-#define ARG5 "dataset-MH01_mono"
-
-// int main(int argc, char **argv) {
-int test(string arg1 = ARG1, string arg2 = ARG2, string arg3 = ARG3,
-         string arg4 = ARG4, string arg5 = ARG5) {
-  //   if (argc < 5) {
-  //     cerr
-  //         << endl
-  //         << "Usage: ./mono_euroc path_to_vocabulary path_to_settings "
-  //            "path_to_sequence_folder_1 path_to_times_file_1 "
-  //            "(path_to_image_folder_2 path_to_times_file_2 ... "
-  //            "path_to_image_folder_N path_to_times_file_N)
-  //            (trajectory_file_name)"
-  //         << endl;
-  //     return 1;
-  //   }
-
-  //   const int num_seq = (argc - 3) / 2;
-  const int num_seq = 1;
-  //   cout << "num_seq = " << num_seq << endl;
-  //   bool bFileName = (((argc - 3) % 2) == 1);
-  string file_name = arg5;
-  //   if (bFileName) {
-  //   file_name = string(argv[argc - 1]);
-  //   file_name = string(argv[argc - 1]);
-  //   cout << "file name: " << file_name << endl;
-  //   }
-
-  // Load all sequences:
-  int seq = 0;
-  vector<vector<string>> vstrImageFilenames;
-  vector<vector<double>> vTimestampsCam;
-  vector<int> nImages;
-
-  vstrImageFilenames.resize(num_seq);
-  vTimestampsCam.resize(num_seq);
-  nImages.resize(num_seq);
-
-  int tot_images = 0;
-  //   for (seq = 0; seq < num_seq; seq++) {
-  cout << "Loading images for sequence " << seq << "...";
-  // LoadImages(string(argv[(2 * seq) + 3]) + "/mav0/cam0/data",
-  //            string(argv[(2 * seq) + 4]), vstrImageFilenames[seq],
-  //            vTimestampsCam[seq]);
-  LoadImages(arg3 + "/mav0/cam0/data", arg4, vstrImageFilenames[seq],
-             vTimestampsCam[seq]);
-  cout << "LOADED!" << endl;
-
-  nImages[seq] = vstrImageFilenames[seq].size();
-  tot_images += nImages[seq];
-  //   }
-
-  // Vector for tracking time statistics
-  vector<float> vTimesTrack;
-  vTimesTrack.resize(tot_images);
-
-  cout << endl << "-------" << endl;
-  cout.precision(17);
-
-  // Create SLAM system. It initializes all system threads and gets ready to
-  // process frames.
-  //   ORB_SLAM3::System SLAM(argv[1], argv[2], ORB_SLAM3::System::MONOCULAR,
-  //   true);
-  ORB_SLAM3::System SLAM(arg1, arg2, ORB_SLAM3::System::MONOCULAR, true);
-
-  for (seq = 0; seq < num_seq; seq++) {
-    // Main loop
-    cv::Mat im;
-    int proccIm = 0;
-    for (int ni = 0; ni < nImages[seq]; ni++, proccIm++) {
-
-      // Read image from file
-      im = cv::imread(vstrImageFilenames[seq][ni], cv::IMREAD_UNCHANGED);
-      double tframe = vTimestampsCam[seq][ni];
-
-      if (im.empty()) {
-        cerr << endl
-             << "Failed to load image at: " << vstrImageFilenames[seq][ni]
-             << endl;
-        return 1;
-      }
-
-      // #ifdef COMPILEDWITHC11
-      std::chrono::steady_clock::time_point t1 =
-          std::chrono::steady_clock::now();
-      // #else
-      //       std::chrono::monotonic_clock::time_point t1 =
-      //           std::chrono::monotonic_clock::now();
-      // #endif
-
-      // Pass the image to the SLAM system
-      // cout << "tframe = " << tframe << endl;
-      SLAM.TrackMonocular(im, tframe); // TODO change to monocular_inertial
-
-      // #ifdef COMPILEDWITHC11
-      std::chrono::steady_clock::time_point t2 =
-          std::chrono::steady_clock::now();
-      // #else
-      //       std::chrono::monotonic_clock::time_point t2 =
-      //           std::chrono::monotonic_clock::now();
-      // #endif
-
-      double ttrack =
-          std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1)
-              .count();
-
-      vTimesTrack[ni] = ttrack;
-
-      // Wait to load the next frame
-      double T = 0;
-      if (ni < nImages[seq] - 1)
-        T = vTimestampsCam[seq][ni + 1] - tframe;
-      else if (ni > 0)
-        T = tframe - vTimestampsCam[seq][ni - 1];
-
-      if (ttrack < T)
-        usleep((T - ttrack) * 1e6); // 1e6
-    }
-
-    if (seq < num_seq - 1) {
-      cout << "Changing the dataset" << endl;
-
-      SLAM.ChangeDataset();
-    }
-  }
-  // Stop all threads
-  SLAM.Shutdown();
-
-  // Save camera trajectory
-  //   if (bFileName) {
-  //     const string kf_file = "kf_" + string(argv[argc - 1]) + ".txt";
-  //     const string f_file = "f_" + string(argv[argc - 1]) + ".txt";
-  //     SLAM.SaveTrajectoryEuRoC(f_file);
-  //     SLAM.SaveKeyFrameTrajectoryEuRoC(kf_file);
-  //   } else {
-  //     SLAM.SaveTrajectoryEuRoC("CameraTrajectory.txt");
-  //     SLAM.SaveKeyFrameTrajectoryEuRoC("KeyFrameTrajectory.txt");
-  //   }
-
-  return 0;
-}
-
-void LoadImages(const string &strImagePath, const string &strPathTimes,
-                vector<string> &vstrImages, vector<double> &vTimeStamps) {
-  ifstream fTimes;
-  fTimes.open(strPathTimes.c_str());
-  vTimeStamps.reserve(5000);
-  vstrImages.reserve(5000);
-  while (!fTimes.eof()) {
-    string s;
-    getline(fTimes, s);
-    if (!s.empty()) {
-      stringstream ss;
-      ss << s;
-      vstrImages.push_back(strImagePath + "/" + ss.str() + ".png");
-      double t;
-      ss >> t;
-      vTimeStamps.push_back(t / 1e9);
-    }
-  }
-}
 
 class System {
 public:
-  // SLAM(string arg1 = ARG1, string arg2 = ARG2, string arg3 = ARG3) {
-  System(const string arg1 = ARG1, const string arg2 = ARG2,
-         const string arg3 = ARG3, const string arg4 = ARG4,
-         const string arg5 = ARG5)
-
-      : SLAM_System(arg1, arg2, ORB_SLAM3::System::MONOCULAR, true) {
-
-    const int num_seq = 1;
-    //   cout << "num_seq = " << num_seq << endl;
-    //   bool bFileName = (((argc - 3) % 2) == 1);
-    string file_name = arg5;
-    //   if (bFileName) {
-    //   file_name = string(argv[argc - 1]);
-    //   file_name = string(argv[argc - 1]);
-    //   cout << "file name: " << file_name << endl;
-    //   }
-
-    // Load all sequences:
-    int seq = 0;
-
-    vstrImageFilenames.resize(num_seq);
-    vTimestampsCam.resize(num_seq);
-    nImages.resize(num_seq);
-
-    int tot_images = 0;
-    //   for (seq = 0; seq < num_seq; seq++) {
-    cout << "Loading images for sequence " << seq << "...";
-    // LoadImages(string(argv[(2 * seq) + 3]) + "/mav0/cam0/data",
-    //            string(argv[(2 * seq) + 4]), vstrImageFilenames[seq],
-    //            vTimestampsCam[seq]);
-    LoadImages(arg3 + "/mav0/cam0/data", arg4, vstrImageFilenames[seq],
-               vTimestampsCam[seq]);
-    cout << "LOADED!" << endl;
-
-    nImages[seq] = vstrImageFilenames[seq].size();
-    tot_images += nImages[seq];
-    //   }
-
-    // Vector for tracking time statistics
-    vTimesTrack.resize(tot_images);
-
-    cout << endl << "-------" << endl;
-    cout.precision(17);
-  }
+  System(std::string arg1 = ARG1, std::string arg2 = ARG2)
+      : SLAM_System(arg1, arg2, ORB_SLAM3::System::MONOCULAR, true) {}
 
   ~System() { SLAM_System.Shutdown(); }
 
-  py::array step(py::array_t<uint8_t> &img) {
-    cv::Mat img2 = CVT::nparray_to_mat(img);
-    // return CVT::mat_to_nparray(img2);
+  py::array step(py::array_t<uint8_t> &img, double tframe) {
+    cv::Mat im = CVT::nparray_to_mat(img);
 
-    cv::Mat im;
-    int proccIm = 0;
-    int seq = 0;
-    for (int ni = 0; ni < nImages[seq]; ni++, proccIm++) {
-
-      // Read image from file
-      im = cv::imread(vstrImageFilenames[seq][ni], cv::IMREAD_UNCHANGED);
-      double tframe = vTimestampsCam[seq][ni];
-
-      if (im.empty()) {
-        cerr << endl
-             << "Failed to load image at: " << vstrImageFilenames[seq][ni]
-             << endl;
-        // return 1;
-        return py::array();
-      }
-
-      // #ifdef COMPILEDWITHC11
-      std::chrono::steady_clock::time_point t1 =
-          std::chrono::steady_clock::now();
-      // #else
-      //       std::chrono::monotonic_clock::time_point t1 =
-      //           std::chrono::monotonic_clock::now();
-      // #endif
-
-      // Pass the image to the SLAM system
-      // cout << "tframe = " << tframe << endl;
-      SLAM_System.TrackMonocular(im,
-                                 tframe); // TODO change to monocular_inertial
-
-      // #ifdef COMPILEDWITHC11
-      std::chrono::steady_clock::time_point t2 =
-          std::chrono::steady_clock::now();
-      // #else
-      //       std::chrono::monotonic_clock::time_point t2 =
-      //           std::chrono::monotonic_clock::now();
-      // #endif
-
-      double ttrack =
-          std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1)
-              .count();
-
-      vTimesTrack[ni] = ttrack;
-
-      // Wait to load the next frame
-      double T = 0;
-      if (ni < nImages[seq] - 1)
-        T = vTimestampsCam[seq][ni + 1] - tframe;
-      else if (ni > 0)
-        T = tframe - vTimestampsCam[seq][ni - 1];
-
-      if (ttrack < T)
-        usleep((T - ttrack) * 1e6); // 1e6
+    if (im.empty()) {
+      std::cerr << std::endl << "empty image" << std::endl;
+      return py::array();
     }
 
-    // if (seq < num_seq - 1) {
-    //   cout << "Changing the dataset" << endl;
+    SLAM_System.TrackMonocular(im, tframe);
 
-    //   SLAM_System.ChangeDataset();
-    // }
-    return CVT::mat_to_nparray(img2);
+    return CVT::mat_to_nparray(im);
   }
 
 private:
   ORB_SLAM3::System SLAM_System;
-  vector<vector<string>> vstrImageFilenames;
-  vector<vector<double>> vTimestampsCam;
-  vector<int> nImages;
-  vector<float> vTimesTrack;
 };
 
 PYBIND11_MODULE(orbslam, m) {
@@ -337,24 +40,10 @@ PYBIND11_MODULE(orbslam, m) {
         ORB_SLAM3 python binding
     )pbdoc";
 
-  m.def("run_test", &test, R"pbdoc(
-        ORB_SLAM3 test
-    )pbdoc",
-        py::arg("vac_file") = ARG1, py::arg("config_file") = ARG2,
-        py::arg("data_path") = ARG3, py::arg("timestamp_file") = ARG4,
-        py::arg("data_name") = ARG5);
-
   py::class_<System>(m, "System")
-      .def(py::init<const string, const string, const string>(),
-           py::arg("vac_file") = ARG1, py::arg("config_file") = ARG2,
-           py::arg("data_path") = ARG3)
-      // .def("step", &SLAM_Bind::step, py::arg("input_image"));
+      .def(py::init<const string, const string>(), py::arg("vacabulary") = ARG1,
+           py::arg("config") = ARG2)
       .def("step", &System::step);
-  //     m.def("subtract", [](int i, int j) { return i - j; }, R"pbdoc(
-  //         Subtract two numbers
-
-  //         Some other explanation about the subtract function.
-  //     )pbdoc");
 
   // #ifdef VERSION_INFO
   //     m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
--- tests/test.py
+++ tests/test.py
@@ -1,27 +1,29 @@
 # -*- coding: utf-8 -*-
-
-# import cmake_example as m
-
-# assert m.__version__ == "0.0.1"
-# assert m.add(1, 2) == 3
-# assert m.subtract(1, 2) == -1
-
+import pdb
 import cv2
-import orbslam
 import glob
+import time
 import numpy as np
-data_path = "extern/ORB_SLAM3/data/MH_01_easy"
-timestamp_path = "extern/ORB_SLAM3/Examples/Monocular/EuRoC_TimeStamps/MH01.txt"
+
+import orbslam
+
+data_path = "./extern/ORB_SLAM3/data/MH_01_easy"
+timestamp_path = "./extern/ORB_SLAM3/Examples/Monocular/EuRoC_TimeStamps/MH01.txt"
 data_name = "dataset-MH01_mono"
+
+images = []
+for filename in glob.glob(data_path + "/mav0/cam0/data/*"):
+    # images.append(cv2.imread(filename, cv2.IMREAD_UNCHANGED))
+    images.append(cv2.imread(filename))
+
+timestamps = np.loadtxt(timestamp_path)
+timestamps = timestamps / 1e9
+nImages = len(timestamps)
+timesTrack = []
 
 s = orbslam.System("extern/ORB_SLAM3/Vocabulary/ORBvoc.txt",
                    "extern/ORB_SLAM3/Examples/Monocular/EuRoC.yaml")
-i = cv2.imread('i.jpg')
-r = s.step(i)
-# images = []
-# for filename in glob.glob(data_path + "/mav0/cam0/data"):
-#     images.append(cv2.imread(filename))
 
-# timestamps = np.loadtxt(timestamp_path)
-# nImages = len(images)
-# vTimesTrack = []
+for i in range(nImages):
+    timesTrack.append(s.step(images[i], timestamps[i]))
+    # TODO 时间控制

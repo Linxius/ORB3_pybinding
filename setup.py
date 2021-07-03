830b55a3d25399cb5f7f8931fb72cd18b55c0f57
Linxius <liniquie@gmail.com>
2021-07-04 00:58:49 +0800

    slam step return camera pos, map points with normals

--- setup.py
+++ setup.py
@@ -104,19 +104,6 @@ class CMakeBuild(build_ext):
         )
 
 
-# The information here can also be placed in setup.cfg - better separation of
-# logic and declaration, and simpler if you include description/version in a file.
-# setup(
-#     name="cmake_example",
-#     version="0.0.1",
-#     author="Dean Moldovan",
-#     author_email="dean0x7d@gmail.com",
-#     description="A test project using pybind11 and CMake",
-#     long_description="",
-#     ext_modules=[CMakeExtension("cmake_example")],
-#     cmdclass={"build_ext": CMakeBuild},
-#     zip_safe=False,
-# )
 setup(
     name="orbslam",
     version="0.0.1",
--- tests/test.py
+++ tests/test.py
@@ -19,11 +19,16 @@ for filename in glob.glob(data_path + "/mav0/cam0/data/*"):
 timestamps = np.loadtxt(timestamp_path)
 timestamps = timestamps / 1e9
 nImages = len(timestamps)
-timesTrack = []
+camera_poses = []
 
 s = orbslam.System("extern/ORB_SLAM3/Vocabulary/ORBvoc.txt",
                    "extern/ORB_SLAM3/Examples/Monocular/EuRoC.yaml")
 
+map_points = []
 for i in range(nImages):
-    timesTrack.append(s.step(images[i], timestamps[i]))
+    camera_pose = s.step(images[i], timestamps[i])
+    map_points = s.get_map_points()
+    # vPos = map_points[:][0]
+    # vNormal = map_points[:][1]
+    pdb.set_trace()
     # TODO 时间控制
